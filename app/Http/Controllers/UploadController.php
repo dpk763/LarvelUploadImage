1d9afaf8e01337d9e9ea7f7638a7e5559bc48eb3
DEEPAK <deepakmr815@gmail.com>
2023-10-07 08:30:35 +0530

    initial commit

---
+++ app/Http/Controllers/UploadController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\UploadImage;
+
+class UploadController extends Controller
+{
+    public function upload(Request $request){
+        // echo "<pre>";
+        // print_r($request->all());
+        $fileName = time()."img.".$request->file('image')->getClientOriginalExtension();
+        $request->file('image')->storeAs('uploads',$fileName);
+
+        $data = new UploadImage();
+        $data->url=$fileName;
+        $data->save();
+
+        return redirect('/upload');
+
+    }
+}
